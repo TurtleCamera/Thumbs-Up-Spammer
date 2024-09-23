01a8ca09ef632394f6e090bd57cdaa6462e24c38
TurtleCamera <80860454+TurtleCamera@users.noreply.github.com>
2024-09-23 14:06:41 -0700

    Added the option to use Unicode to make the text smaller

--- Spammer.py
+++ Spammer.py
@@ -3,19 +3,41 @@ import argparse
 
 # Set up command-line argument parsing
 parser = argparse.ArgumentParser(description="Generate a :thumbsup: emoji spam string.")
-parser.add_argument("-s", "--start_counter", default=1, type=int, help="The counter to start at")
-parser.add_argument("-c", "--count", default=199, type=int, help="The number of times you wish to spam the :thumbsup: emoji")
+parser.add_argument("-s", "--start_counter", default=1, type=int, help="The counter to start at.")
+parser.add_argument("-c", "--count", default=199, type=int, help="The number of times you wish to spam the :thumbsup: emoji.")
+parser.add_argument("-u", "--unicode", action="store_true", help="Use unicode superscript to make the text smaller.")
 
 args = parser.parse_args()
 
 start_counter = args.start_counter
-count = min(args.count, 199)  # Limit to 199 emojis
+use_unicode = args.unicode
+count = min(args.count, 198 if use_unicode else 199)  # Limit to 199 emojis if normal and 198 if unicode
 
-# Generate the spam string
-spam_messages = [f"👍 X{i}" for i in range(start_counter, start_counter + count)]
+print(use_unicode)
 
-# Join the messages with commas and 'and' for the last one
-spam_string = ', '.join(spam_messages[:-1]) + ', and ' + spam_messages[-1]
+# Generate the spam string with or without superscripted counter
+if use_unicode:
+    # Unicode superscripts for numbers 0-9
+    superscripts = str.maketrans('0123456789', '⁰¹²³⁴⁵⁶⁷⁸⁹')
+
+    # Function to convert a number to superscript
+    def to_superscript(num):
+        return str(num).translate(superscripts)
+    
+    # Create the :thumbsup: emojis with counters
+    spam_messages = [f"👍 ˣ{to_superscript(i)}" for i in range(start_counter, start_counter + count)]
+
+    # Join the messages with commas and 'and' for the last one
+    spam_string = ', '.join(spam_messages[:-1]) + ', and ' + spam_messages[-1]
+
+    # Append "-#" to make it appear smaller on Discord
+    spam_string = "-# " + spam_string
+else:
+    # Create the :thumbsup: emojis with counters
+    spam_messages = [f"👍 X{i}" for i in range(start_counter, start_counter + count)]
+
+    # Join the messages with commas and 'and' for the last one
+    spam_string = ', '.join(spam_messages[:-1]) + ', and ' + spam_messages[-1]
 
 # Copy the generated string to the clipboard
 pyperclip.copy(spam_string)
